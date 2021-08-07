9e7a24e72938f3c9c98e1b35897dee4e44a091aa
tchukina <tatiana.zhabenko@gmail.com>
2021-08-07 23:56:33 +0300

    Added sentry settings, added babel settings

--- src/PizzaConfigurator.js
+++ src/PizzaConfigurator.js
@@ -1,16 +1,14 @@
 import React, {Component} from "react";
 import PizzaOrder from "./PizzaOrder.js";
 
-export const error = new Error('Something went wrong');
 class PizzaConfigurator extends Component {
     render() {
         return (
             <div className="App-header">
                 <h1>STAGING</h1>
+                <button onClick={() => {throw new Error("Some Error")}}>Break the world</button>
                 <div><h1>Собери свою пиццу</h1></div>
                 <PizzaOrder/>
-                <hr/>
-                <button onClick={() => throw error}>Break the world</button>;
             </div>
         )
     }
