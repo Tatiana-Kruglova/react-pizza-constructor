3ea137cef353b46e4f13a3cf232402da11cd9758
tchukina <tatiana.zhabenko@gmail.com>
2021-08-07 22:46:39 +0300

    Added staging header

--- src/PizzaConfigurator.js
+++ src/PizzaConfigurator.js
@@ -5,6 +5,7 @@ class PizzaConfigurator extends Component {
     render() {
         return (
             <div className="App-header">
+                <h1>STAGING</h1>
                 <div><h1>Собери свою пиццу</h1></div>
                 <PizzaOrder/>
             </div>
